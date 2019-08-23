b0e8504340406d72428443e92fb0f1a336873841
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2019-08-23 13:44:56 +0200

    added es5.yaspl

--- src/scripts/target.c
+++ src/scripts/target.c
@@ -12,10 +12,32 @@ typedef char string[STRING_CONST];
 string yasplBuffer;
 string toParse;
 
-int num,res,sum = 0;
-bool flag = false;
+int opt,n1,n2,res = 0;
+void somma(int op1, int op2, int *res){
+*res = 0;
+*res = op1 + op2;
+}
+void moltiplicazione(int op1, int op2, int *res){
+int i = 0;
+*res = 0;
+while(i < op2){
+*res = *res + op1;
+i = i + 1;
+
+}
+}
+void potenza(int b, int e, int *res){
+int i = 1;
+*res = 1;
+while(i <= e){
+*res = *res * b;
+i = i + 1;
+
+}
+}
 void fibonacci(int num){
 int i = 1,a = -1,b = 1;
+res = 0;
 while(i <= num){
 res = a + b;
 a = b;
@@ -25,39 +47,107 @@ printf("%d\n",res);
 
 }
 }
-void sommaFibonacci(int num, int *sum){
-int i = 1,a = -1,b = 1;
-while(i <= num){
-res = a + b;
-a = b;
-b = res;
-i = i + 1;
-*sum = *sum + res;
+void menu(){
 
-}
-}
-void checkInput(int x, bool *flag){
-if(x >= 0){
-*flag = true;
-}
-else{
-*flag = false;
-}
+strcpy(yasplBuffer,"Scegli un opzione:\n");
+strcat(yasplBuffer, "1. Somma fra due interi\n");
+strcpy(toParse,"2. Moltiplicazione fra due interi\n");
+strcat(yasplBuffer, toParse);
+strcpy(toParse,"3. Elevazione a potenza\n");
+strcat(yasplBuffer, toParse);
+strcpy(toParse,"4. Successione di Fibonacci\n");
+strcat(yasplBuffer, toParse);
+strcpy(toParse,"0. Esci");
+strcat(yasplBuffer, toParse);
+
+printf("%s\n", yasplBuffer);
+
 }
 
 int main(void){
-printf("%s\n","Quanti numeri di Fibonacci vuoi vedere");
-scanf("%d",&num);
-checkInput(num,&flag);
-if(flag == true){
-printf("%s\n","Questa è la sequenza");
-fibonacci(num);
-printf("%s\n","La somma della sequenza è");
-sommaFibonacci(num,&sum);
-printf("%d\n",sum);
+menu();
+scanf("%d",&opt);
+while(!(opt == 0)){
+if(opt == 1){
+printf("%s\n","Inserisci due interi");
+scanf("%d",&n1);
+scanf("%d",&n2);
+somma(n1,n2,&res);
+
+strcpy(yasplBuffer," + ");
+sprintf(toParse,"%d", n1);
+strcat(toParse, yasplBuffer);
+strcpy(yasplBuffer, toParse);
+sprintf(toParse,"%d",n2);
+strcat(yasplBuffer, toParse);
+strcpy(toParse," = ");
+strcat(yasplBuffer, toParse);
+sprintf(toParse,"%d",res);
+strcat(yasplBuffer, toParse);
+
+printf("%s\n", yasplBuffer);
+
 }
 else{
-printf("%s\n","Inserisci un intero positivo");
+if(opt == 2){
+printf("%s\n","Inserisci due interi");
+scanf("%d",&n1);
+scanf("%d",&n2);
+moltiplicazione(n1,n2,&res);
+
+strcpy(yasplBuffer," * ");
+sprintf(toParse,"%d", n1);
+strcat(toParse, yasplBuffer);
+strcpy(yasplBuffer, toParse);
+sprintf(toParse,"%d",n2);
+strcat(yasplBuffer, toParse);
+strcpy(toParse," = ");
+strcat(yasplBuffer, toParse);
+sprintf(toParse,"%d",res);
+strcat(yasplBuffer, toParse);
+
+printf("%s\n", yasplBuffer);
+
+}
+else{
+if(opt == 3){
+printf("%s\n","Inserisci basa ed esponente");
+scanf("%d",&n1);
+scanf("%d",&n2);
+potenza(n1,n2,&res);
+
+strcpy(yasplBuffer," ^ ");
+sprintf(toParse,"%d", n1);
+strcat(toParse, yasplBuffer);
+strcpy(yasplBuffer, toParse);
+sprintf(toParse,"%d",n2);
+strcat(yasplBuffer, toParse);
+strcpy(toParse," = ");
+strcat(yasplBuffer, toParse);
+sprintf(toParse,"%d",res);
+strcat(yasplBuffer, toParse);
+
+printf("%s\n", yasplBuffer);
+
+}
+else{
+if(opt == 4){
+printf("%s\n","Inserisci il numero per vedere la successione di Fibonacci");
+scanf("%d",&n1);
+if(n1 >= 0){
+printf("%s\n","Questa è la sequenza:");
+fibonacci(n1);
+}
+else{
+printf("%s\n","Inserisci numero positivo");
+}
+}
+}
+}
+}
+menu();
+scanf("%d",&opt);
+
 }
 return 0;
 }
